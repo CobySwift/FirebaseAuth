78cb1ebe6496a2ee95fa3b24c18215d53516f297
kimdoyoung <57849386+coby5502@users.noreply.github.com>
2022-04-11 21:25:38 +0900

    Initial Commit

---
+++ Firebase_Auth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Firebase_Auth
+//
+//  Created by COBY_PRO on 2022/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Firebase_Auth/Firebase_AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_AuthApp.swift
+//  Firebase_Auth
+//
+//  Created by COBY_PRO on 2022/04/11.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
